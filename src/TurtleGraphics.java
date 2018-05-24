bf043faca9f06eaa8b78289be4d176aabca94d93
Ian <ismoore@mix.wvu.edu>
2018-05-24 12:36:47 -0400

    Changed some inheritance

--- src/TurtleGraphics.java
+++ src/TurtleGraphics.java
@@ -7,13 +7,21 @@ import java.util.Scanner;
 public class TurtleGraphics {
 	public static void main(String[] args){
 	
-	//starts at top left (0, 0)
+	//position starts at top left (0, 0)
 	int xCoord = 0; // the x or row coordinate the turtle is at
 	int yCoord = 0; // the y or column coordinate the turtle is at
 	int arraySize = 10; // size of the array the turtle moves in
 	int turtleArray[][] = new int[arraySize][arraySize]; // the array the turtle's path will be in
 	
+	int menuEnd = 0; // used for the menu while loop
+	int firstSelection; // the initial user selection within the loop
+	int selectDirection; // the direction the user selects
+	int selectDistance = 0; // the distance the user selects
 	Direction dir = Direction.right;//direction enum, start off facing right
+	
+	//constructors
+	TurtleMover turt = new TurtleMover(selectDistance, dir, xCoord, yCoord, turtleArray, arraySize);
+	PathPainter paint = new PathPainter(turtleArray, arraySize);
 	
 	//fill in array with 0s initially
 	for(int i = 0; i<arraySize; i++){
@@ -22,54 +30,50 @@ public class TurtleGraphics {
 				}
 			}
 	
-	//menu
-	int menuEnd = 0; // used for the menu while loop
-	int firstSelection; // the initial user selection within the loop
-	int selectDirection; // the direction the user selects
-	int selectDistance = 0; // the distance the user selects
-	TurtleMover turt = new TurtleMover(selectDistance, dir, xCoord, yCoord, turtleArray, arraySize);
-			
-			while (menuEnd!=1)
-			{
-				//ask for input
-				System.out.println("Press 1 to change direction\n"
-						+ "Press 2 to move forward\n"
-						+ "Press 3 to print the current map\n"
-						+ "Press 4 to end\n\n");
+			//menu
+	while (menuEnd!=1){
+		
+		//ask for input
+		System.out.println("Press 1 to change direction\n"
+					+ "Press 2 to move forward\n"
+					+ "Press 3 to print the current map\n"
+					+ "Press 4 to end\n\n");
 				
-				Scanner input = new Scanner(System.in);
-				firstSelection = input.nextInt();
+		Scanner input = new Scanner(System.in);
+		firstSelection = input.nextInt();
 				
-				//check and respond to user selection
-				switch (firstSelection)
-				{
-				case 1:	System.out.println("Which direction? Enter: 1 for up, 2 for right, 3 for down, 4 for left\n");
-						selectDirection = input.nextInt();
-						//check if the direction is ok and assign such that: 1 = up, 2 = right, 3 = down, 4 = left
-						if (selectDirection == 1) dir = Direction.up;
+		//check and respond to user selection
+		switch (firstSelection){
+		case 1:	System.out.println("Which direction? Enter: 1 for up, 2 for right, 3 for down, 4 for left\n");
+				selectDirection = input.nextInt();
+				//check if the direction is ok and assign such that: 1 = up, 2 = right, 3 = down, 4 = left
+				if (selectDirection == 1) dir = Direction.up;
 						
-						else if (selectDirection == 2) dir = Direction.right;
+				else if (selectDirection == 2) dir = Direction.right;
 						
-						else if (selectDirection == 3) dir = Direction.down;
+				else if (selectDirection == 3) dir = Direction.down;
+					
+				else if (selectDirection == 4) dir = Direction.left;
 						
-						else if (selectDirection == 4) dir = Direction.left;
+				else System.out.println("Incorrect selection: Must be 1, 2, 3, or 4\n");
 						
-						else System.out.println("Incorrect selection: Must be 1, 2, 3, or 4\n");
+				break;
+		case 2: System.out.println("How far would you like to move? Remember to stay within bounds\n"
+				+ "Current Position: (" +xCoord+ ", " + yCoord + ")\n"
+				+ "Size of Matrix: " + arraySize + "\n");
+		
+		        selectDistance = input.nextInt();
 						
-						break;
-				case 2: System.out.println("How far would you like to move? Remember to stay within bounds\n");
-						selectDistance = input.nextInt();
+				//update turtle with desired distance
+				turtleArray = turt.moveTurtle(selectDistance, dir, xCoord, yCoord, turtleArray, arraySize);
 						
-						//update turtle with desired distance
-						turtleArray = turt.moveTurtle(selectDistance, dir, xCoord, yCoord, turtleArray, arraySize);
+				xCoord = turt.updateXCoord(dir, xCoord, selectDistance);
 						
-						xCoord = turt.updateXCoord(dir, xCoord, selectDistance);
+				yCoord = turt.updateYCoord(dir, yCoord, selectDistance);
 						
-						yCoord = turt.updateYCoord(dir, yCoord, selectDistance);
-						
-						break;
-				case 3: turt.paintPath(turtleArray, arraySize);
-						break;
+				break;
+		case 3: paint.paintPath(turtleArray, arraySize);
+					break;
 				case 4: menuEnd = 1;
 						break;
 				default: System.out.println("Not a valid choice\n");
@@ -79,5 +83,6 @@ public class TurtleGraphics {
 			}
 	
 	}
+
 
 }
--- src/TurtleMover.java
+++ src/TurtleMover.java
@@ -15,7 +15,7 @@ public class TurtleMover extends TurtleGraphics{
 	}
 	
 	
-	/*moves the turtle in the desired direction and distance
+	/* moves the turtle in the desired direction and distance
 	 * calls isValidMove to check if it is within bounds
 	 * updates array to have a 1 in the path the turtle took
 	 * returns updated array
@@ -25,6 +25,7 @@ public class TurtleMover extends TurtleGraphics{
 		int rowPos = yCoord;
 		int colPos = xCoord;
 		int[][] array = arr;
+		
 		//if move is valid
 		if (isValidMove(distance, direction, xCoord, yCoord, arraySize) == true){
 			
@@ -122,27 +123,5 @@ public class TurtleMover extends TurtleGraphics{
 		else return false;
 	}
 	
-	
-	//prints out the path, if a 1 is encountered, it prints a '*' symbol
-	public void paintPath(int[][] array, int arraySize){
-		int length =0;
-		
-		//checks each position in the array and prints a * if a 1 is encountered
-		for (int i = 0; i<arraySize; i++){
-			for (int j = 0; j<arraySize; j++){
-				//new line if at end of array
-				if (length>=arraySize){
-					System.out.print("\n");
-					length = 0;
-				}
-				
-				if (array[i][j] == 1) System.out.print("*");
-				
-				if (array[i][j] == 0) System.out.print(" ");
-				
-				length++;
-			}
-		}
-	}
 
 }
